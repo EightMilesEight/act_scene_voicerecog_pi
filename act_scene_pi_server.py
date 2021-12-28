3c50c384dd0958ad2922552032788e97d13d4056
EightMilesEight <65874539+EightMilesEight@users.noreply.github.com>
2021-12-28 10:43:50 -0500

    Final Version Part 1 (untested with seven-segment display)
    Commented out code written for displaying on Adafruit seven-segment display. Those sections are currently untested because I can't get my hands on one due to supply chain issues.

--- act_scene_pi_server.py
+++ act_scene_pi_server.py
@@ -1,5 +1,25 @@
+# pip3 install adafruit-blinka
+# lsmod | grep -i i2c
+# sudo i2cdetect -y 1
+# pip3 install adafruit-circuitpython-ht16k33
 import socket
 from threading import *
+'''
+import time
+import board
+import busio
+from adafruit_ht16k33 import segments
+
+# Create the I2C interface.
+i2c = busio.I2C(board.SCL, board.SDA)
+
+# Create the LED segment class.
+# This creates a 7 segment 4 character display:
+display = segments.Seg7x4(i2c)
+
+# Clear the display.
+display.fill(0)
+'''
 
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = "localhost"
@@ -16,12 +36,17 @@ class client(Thread):
         self.start()
 
     def run(self):
-        while 1:
-            print('Client sent:', self.sock.recv(1024).decode())
+        while True:
+            act_scene = self.sock.recv(1024).decode()
+            print('Client sent:',act_scene)
+            '''
+            display.print(':')
+            display.print(act_scene)
+            '''
             self.sock.send(b'Recieved')
 
 serversocket.listen(5)
 print ('server started and listening')
-while 1:
+while True:
     clientsocket, address = serversocket.accept()
     client(clientsocket, address)
