d953fadc8e423f86735a977898635245837559fa
EightMilesEight <65874539+EightMilesEight@users.noreply.github.com>
2022-05-20 11:10:11 -0400

    Completely Refactored Socket
    Server is now the device recognizing, Client is now the display device.

--- act_scene_client.py
+++ act_scene_client.py
@@ -1,170 +1,31 @@
 import socket
+from threading import *
+from tkinter import *
 import time
 
-# pip install SpeechRecognition
-# in case of error use 'pip install pyaudio' or...
-# in case of error use 'pip install pipwin' then 'pipwin install pyaudio'
-# if error continued you may need to use python 3.6 or lower as the latest
-# python may not support pyaudio...
-import speech_recognition as sr
-
-# Import tkinter library
-from tkinter import *
-
-import sys
-
-sys.stdout = open('log.txt', 'w+')
+'''
+import time
+import board
+import busio
+from adafruit_ht16k33 import segments
+# Create the I2C interface.
+i2c = busio.I2C(board.SCL, board.SDA)
+# Create the LED segment class.
+# This creates a 7 segment 4 character display:
+display = segments.Seg7x4(i2c)
+# Clear the display.
+display.fill(0)
+'''
 
 version = 'Beta v1.3'
-speech = ''
-r = 'Unsure'
 status = ''
-markers = {}
 
-
-def open_lines():
-    text_box.delete(0.0, END)
-    lines = open("lines.txt", "r")
-    text_box.insert(END, lines.read())
-    lines.close()
-
-
-def save_lines():
-    global current_screen
-    lines = open("lines.txt", "w")
-    lines.write(text_box.get(1.0, END))
-    lines.close()
-    text_box.destroy()
-    open_btn.destroy()
-    save.destroy()
-    current_screen = 1
-
-
-def GUI_start():
-    global win
-    global text_box
-    global open_btn
-    global save
-    global speech_lbl
-    global scene_lbl
-    global status_lbl
-    global current_screen
-
-    current_screen = 0
-
-    # Create an instance of tkinter window
-    win = Tk()
-    win.title('Scene Recognizer ' + version)
-
-    # Creating a text box widget
-    text_box = Text(win)
-    text_box.pack()
-
-    open_btn = Button(win, text="Reset to Previous Lines", command=open_lines)
-    open_btn.pack()
-
-    # Create a button to save the text
-    save = Button(win, text="Save Lines", command=save_lines)
-    save.pack()
-
-    open_lines()
-
-    while True:
-        if current_screen == 0:
-            win.update()
-        elif current_screen == 1:
-            speech_lbl = Label(win, text='Speech recognized: ' + speech)
-            speech_lbl.pack()
-            scene_lbl = Label(win, text='Current scene: ' + r)
-            scene_lbl.pack()
-            status_lbl = Label(win, text='Current status: ' + status)
-            status_lbl.pack()
-            win.update()
-            break
-
-
-# creates a list called "lines" out of lines.txt and strips special characters
-with open('lines.txt', 'r') as file:
-    lines = file.readlines()
-    lines = [line.rstrip() for line in lines]
-    lines = list(filter(None, lines))
-
-
-# check if the speech includes a phrase, and return which phrase is included
-# the first phrase in the list that is recognized will be returned
-# for some reason it has an error with - and ' so until I fix that don't use lines containing them
-def check(phrase):
-    for line in lines:
-        if line in phrase:
-            return line
-
-
-def create_markers():
-    y = 0
-    x = 0
-    while True:
-        try:
-            x += 1
-            marker = 'act %d scene %d' % (y, x)
-            markers[marker] = lines.index(marker)
-        except ValueError:
-            if y <= 10:
-                y += 1
-                x = 0
-            else:
-                marker = '<end>'
-                markers[marker] = lines.index(marker)
-                break
-
-
-def find_scene():
-    for i in list(markers.values()):
-        if lines.index(check(speech)) in range(i, list(markers.values())[list(markers.values()).index(i) + 1]):
-            return '0'+list(markers)[list(markers.values()).index(i)][4]+list(markers)[list(markers.values()).index(i)][-2].replace(' ', '0')+list(markers)[list(markers.values()).index(i)][-1].replace(' ', '0')
-
-# simple function to recognise speech from user
-def takecommand():
-    # it takes microphone input and returns string output
-    r = sr.Recognizer()
-    with sr.Microphone(device_index=1) as source:
-        status = 'Listening...'
-        status_lbl.configure(text='Current status: ' + status)
-        win.update()
-        print(status)
-        r.pause_threshold = 0.5
-        audio = r.listen(source)
-    try:
-        status = 'Recognizing...'
-        status_lbl.configure(text='Current status: ' + status)
-        win.update()
-        print(status)
-        speech = r.recognize_google(audio, language='en-in')
-        speech_lbl.configure(text='Speech recognized: : ' + speech)
-        win.update()
-        print('Speech recognized: ', speech)
-    except:
-        print('Error: No recognizable speech detected')
-        return "None"
-    return speech
-
-def ts(str):
-    while True:
-        try:
-            s.sendall(r.encode('utf-8'))
-            data = ''
-            data = s.recv(1024).decode()
-            print(data)
-            break
-        except:
-            status = 'Error: server offline (reconnecting in 10s)'
-            status_lbl.configure(text='Current status: ' + status)
-            win.update()
-            print(status)
-            time.sleep(10)
-
-
-create_markers()
-GUI_start()
+act_scene = 'Unsure'
+win = Tk()
+win.title('Scene Recognizer ' + version)
+scene_lbl = Label(win, text='Current scene: ' + act_scene, font=('Arial'))
+scene_lbl.pack()
+win.update()
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = "localhost"
 port = 1024
@@ -173,22 +34,14 @@ while True:
         s.connect((host, port))
         break
     except:
-        status = 'Error: server offline (reconnecting in 10s)'
-        status_lbl.configure(text='Current status: ' + status)
-        win.update()
-        print(status)
-        time.sleep(10)
-# creates a list called "lines" out of lines.txt and strips special characters
-with open('lines.txt') as file:
-    lines = file.readlines()
-    lines = [line.rstrip() for line in lines]
-    lines = list(filter(None, lines))
+        time.sleep(3)
 while True:
-    speech = takecommand()  # whatever user says will be stored in this variable
-    if check(speech) != None:
-        print(check(speech))
-        r = find_scene()
-        scene_lbl.configure(text='Current scene: ' + r)
-        win.update()
-        print(r)
-        ts(s)
+    act_scene = s.recv(1024).decode()
+    print('Client sent:', act_scene)
+    scene_lbl.configure(text='Current scene: ' + act_scene)
+    win.update()
+    '''
+    display.print(':')
+    display.print(act_scene)
+    '''
+    s.sendall(b'Recieved')
--- act_scene_server.py
+++ act_scene_server.py
@@ -1,27 +1,21 @@
-# pip3 install adafruit-blinka
-# lsmod | grep -i i2c
-# sudo i2cdetect -y 1
-# pip3 install adafruit-circuitpython-ht16k33
 import socket
-from threading import *
-from tkinter import *
-'''
 import time
-import board
-import busio
-from adafruit_ht16k33 import segments
-# Create the I2C interface.
-i2c = busio.I2C(board.SCL, board.SDA)
-# Create the LED segment class.
-# This creates a 7 segment 4 character display:
-display = segments.Seg7x4(i2c)
-# Clear the display.
-display.fill(0)
-'''
+from threading import *
 
-version = 'Beta v1.3'
+# pip install SpeechRecognition
+# in case of error use 'pip install pyaudio' or...
+# in case of error use 'pip install pipwin' then 'pipwin install pyaudio'
+# if error continued you may need to use python 3.6 or lower as the latest
+import speech_recognition as sr
+from tkinter import *
 
+import sys
 
+version = 'Beta v1.4'
+speech = ''
+r = 'Unsure'
+status = ''
+markers = {}
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = "localhost"
 port = 1024
@@ -37,32 +31,150 @@ class client(Thread):
         self.addr = address
         self.start()
 
-    def run(self):
-        act_scene = 'Unsure'
+    def open_lines(self):
+        text_box.delete(0.0, END)
+        lines = open("lines.txt", "r")
+        text_box.insert(END, lines.read())
+        lines.close()
+
+    def save_lines(self):
+        global current_screen
+        lines = open("lines.txt", "w")
+        lines.write(text_box.get(1.0, END))
+        lines.close()
+        text_box.destroy()
+        open_btn.destroy()
+        save.destroy()
+        current_screen = 1
+
+    def GUI_start(self):
+        global win
+        global text_box
+        global open_btn
+        global save
+        global speech_lbl
+        global scene_lbl
+        global status_lbl
+        global current_screen
+
+        current_screen = 0
+
+        # Create an instance of tkinter window
         win = Tk()
         win.title('Scene Recognizer ' + version)
-        scene_lbl = Label(win, text='Current scene: ' + act_scene, font=('Arial'))
-        scene_lbl.pack()
-        win.update()
+
+        # Creating a text box widget
+        text_box = Text(win)
+        text_box.pack()
+
+        open_btn = Button(win, text="Reset to Previous Lines", command=self.open_lines)
+        open_btn.pack()
+
+        # Create a button to save the text
+        save = Button(win, text="Save Lines", command=self.save_lines)
+        save.pack()
+
+        self.open_lines()
+
         while True:
-            act_scene = self.sock.recv(1024).decode()
-            print('Client sent:', act_scene)
-            scene_lbl.configure(text='Current scene: ' + act_scene)
+            if current_screen == 0:
+                win.update()
+            elif current_screen == 1:
+                speech_lbl = Label(win, text='Speech recognized: ' + speech)
+                speech_lbl.pack()
+                scene_lbl = Label(win, text='Current scene: ' + r)
+                scene_lbl.pack()
+                status_lbl = Label(win, text='Current status: ' + status)
+                status_lbl.pack()
+                win.update()
+                break
+
+    # check if the speech includes a phrase, and return which phrase is included
+    # the first phrase in the list that is recognized will be returned
+    # for some reason it has an error with - and ' so until I fix that don't use lines containing them
+    def check(self, phrase):
+        for line in lines:
+            if line in phrase:
+                return line
+
+    def create_markers(self):
+        y = 0
+        x = 0
+        while True:
+            try:
+                x += 1
+                marker = 'act %d scene %d' % (y, x)
+                markers[marker] = lines.index(marker)
+            except ValueError:
+                if y <= 10:
+                    y += 1
+                    x = 0
+                else:
+                    marker = '<end>'
+                    markers[marker] = lines.index(marker)
+                    break
+
+    def find_scene(self):
+        for i in list(markers.values()):
+            if lines.index(self.check(speech)) in range(i, list(markers.values())[list(markers.values()).index(i) + 1]):
+                return '0' + list(markers)[list(markers.values()).index(i)][4] + \
+                       list(markers)[list(markers.values()).index(i)][-2].replace(' ', '0') + \
+                       list(markers)[list(markers.values()).index(i)][-1].replace(' ', '0')
+
+    # simple function to recognise speech from user
+    def takecommand(self):
+        # it takes microphone input and returns string output
+        r = sr.Recognizer()
+        with sr.Microphone(device_index=1) as source:
+            status = 'Listening...'
+            status_lbl.configure(text='Current status: ' + status)
             win.update()
-            '''
-            display.print(':')
-            display.print(act_scene)
-            '''
-            self.sock.send(b'Recieved')
+            print(status)
+            r.pause_threshold = 0.5
+            audio = r.listen(source)
+        try:
+            status = 'Recognizing...'
+            status_lbl.configure(text='Current status: ' + status)
+            win.update()
+            print(status)
+            speech = r.recognize_google(audio, language='en-in')
+            speech_lbl.configure(text='Speech recognized: : ' + speech)
+            win.update()
+            print('Speech recognized: ', speech)
+        except:
+            print('Error: No recognizable speech detected')
+            return "None"
+        return speech
+
+    def run(self):
+        global lines
+        global r
+        global status
+        global speech
+        with open('lines.txt') as file:
+            lines = file.readlines()
+            lines = [line.rstrip() for line in lines]
+            lines = list(filter(None, lines))
+        self.create_markers()
+        self.GUI_start()
+        # creates a list called "lines" out of lines.txt and strips special characters
+        with open('lines.txt') as file:
+            lines = file.readlines()
+            lines = [line.rstrip() for line in lines]
+            lines = list(filter(None, lines))
+        while True:
+            speech = self.takecommand()  # whatever user says will be stored in this variable
+            if self.check(speech) != None:
+                print(self.check(speech))
+                r = self.find_scene()
+                scene_lbl.configure(text='Current scene: ' + r)
+                win.update()
+                print(r)
+                self.sock.send(r.encode("UTF-8"))
+
 
 serversocket.listen(5)
 print ('server started and listening')
-startup_win = Tk()
-startup_win.title('Scene Recognizer Startup ' + version)
-startup_lbl = Label(startup_win, text='Waiting for a client to connect', font=('Arial'))
-startup_lbl.pack()
-startup_win.update()
 while True:
     clientsocket, address = serversocket.accept()
-    startup_win.destroy()
     client(clientsocket, address)
